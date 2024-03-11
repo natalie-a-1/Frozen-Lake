02b0084908cf7d198f36362283e8d4854feb5c97
nataliehill <natalie.a.hill-1@ou.edu>
2024-03-11 18:03:08 -0500

    Start monte carlo

--- monte-carlo.py
+++ monte-carlo.py
@@ -1,69 +1,83 @@
-# import gym
-# import numpy as np
-# import random
+import gym
+import numpy as np
+import random
+import matplotlib.pyplot as plt
 
-# class MonteCarloAgent:
-#     def __init__(self, gamma=0.9, max_episodes=10000, epsilon=0.1):
-#         self.gamma = gamma
-#         self.max_episodes = max_episodes
-#         self.epsilon = epsilon  # Exploration rate
+class MonteCarlo:
+    def __init__(self, env, gamma=0.9, max_episodes=100000, epsilon=1.0, decay_rate=0.0001, min_epsilon=0.001):
+        self.env = env
+        self.gamma = gamma
+        self.max_episodes = max_episodes
+        self.epsilon = epsilon
+        self.decay_rate = decay_rate
+        self.min_epsilon = min_epsilon
 
-#         self.state_dim = env.observation_space.n
-#         self.action_dim = env.action_space.n
+        self.state_dim = env.observation_space.n
+        self.action_dim = env.action_space.n
 
-#         self.q_table = np.zeros((self.state_dim, self.action_dim))
-#         self.returns = {(s, a): [] for s in range(self.state_dim) for a in range(self.action_dim)}
-#         self.policy = np.random.choice(self.action_dim, self.state_dim)
+        self.q_table = np.zeros((self.state_dim, self.action_dim))
+        self.new_q = np.zeros((self.state_dim, self.action_dim))
+        self.reward_list = []
+        self.average_rewards = []  # To store average rewards
 
-#     def generate_episode(self):
-#         episode = []
-#         state_info = env.reset()
-#         state = state_info[0] if isinstance(state_info, tuple) else state_info
-#         done = False
+    def train(self):
+        for episode in range(self.max_episodes):
+            state_info = env.reset()
+            state = state_info[0]
+            done = False
+            episode_rewards = []
+            episode_states_actions = []
+            score = 0
 
-#         while not done:
-#             action = self.get_action(state)
-#             result = env.step(action)
-#             next_state, reward, terminated, truncated, _ = result if len(result) == 5 else result[0], result[1], result[2], result[3], {}
-#             next_state = next_state[0] if isinstance(next_state, tuple) else next_state
-#             episode.append((state, action, reward))
-#             state = next_state
+            while not done:
+                if random.uniform(0, 1) < self.epsilon:
+                    action = self.env.action_space.sample()
+                else:
+                    action = np.argmax(self.q_table[state, :])
+                
+                result = env.step(action)
+                new_state, reward, terminated, truncated, _ = result if len(result) == 5 else result[0], result[1], result[2], result[3], {}
 
-#         return episode
+                episode_states_actions.append((state, action))
+                done = terminated or truncated
+                score += reward
+                state = new_state[0]
+            
+            self.reward_list.append(score)
 
-#     def get_action(self, state):
-#         # Epsilon-greedy policy
-#         if random.uniform(0, 1) < self.epsilon:
-#             return env.action_space.sample()
-#         else:
-#             return np.argmax(self.q_table[state, :])
+            for state, action in episode_states_actions:
+                self.new_q[int(state), action] += 1
+                learning_rate = 1.0 / self.new_q[state, action]
+                self.q_table[state, action] += learning_rate * (score - self.q_table[state, action])
 
-#     def update_q_table(self, episode):
-#         G = 0
-#         for state, action, reward in reversed(episode):
-#             G = self.gamma * G + reward
-#             if not (state, action) in [(x[0], x[1]) for x in episode[:-1]]:
-#                 self.returns[(state, action)].append(G)
-#                 self.q_table[state][action] = np.mean(self.returns[(state, action)])
+            self.epsilon = max(self.min_epsilon, self.epsilon * np.exp(-self.decay_rate * episode))
 
-#     def derive_policy(self):
-#         for state in range(self.state_dim):
-#             self.policy[state] = np.argmax(self.q_table[state])
+            if episode % 1000 == 0:
+                average_reward = sum(self.reward_list[-1000:]) / 1000
+                self.average_rewards.append(average_reward)
+                print(f"Episode {episode}/{self.max_episodes} - Average Reward: {average_reward}")
+        
+        print(f"Final score: {sum(self.reward_list) / self.max_episodes}")
+        self.plot_average_rewards()
 
-#     def train(self):
-#         for episode in range(1, self.max_episodes + 1):
-#             episode_data = self.generate_episode()
-#             self.update_q_table(episode_data)
-#             self.derive_policy()
+    def plot_average_rewards(self):
+        episodes_per_point = 1000  # This should match the frequency of your averaging
+        episodes = np.arange(len(self.average_rewards)) * episodes_per_point
 
-#             if episode % 1000 == 0:
-#                 print(f"Episode {episode}/{self.max_episodes} completed.")
+        plt.figure(figsize=(10, 5))
+        plt.plot(episodes, self.average_rewards, label='Average Reward')
+        plt.title("Average Reward Over Time")
+        plt.xlabel("Episode")
+        plt.ylabel("Average Reward")
 
-# # Example usage
-# env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False)
-# mc_agent = MonteCarloAgent(gamma=0.9, max_episodes=10000, epsilon=0.1)
-# mc_agent.train()
+        # Improve readability of the x-axis by adjusting the number of ticks
+        max_episode = len(self.average_rewards) * episodes_per_point
+        plt.xticks(np.arange(0, max_episode, step=max_episode / 10))  # Adjust step for more or fewer ticks
 
-# print("Derived policy:")
-# print(mc_agent.policy)
-print("hello")
+        plt.legend()
+        plt.grid(True)  # Optional: Adds a grid for easier reading
+        plt.show()
+# Example usage
+env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False)
+mc_agent = MonteCarlo(env)
+mc_agent.train()
